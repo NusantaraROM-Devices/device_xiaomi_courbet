19a382c54caf3229ba240c811df6299aa1fe7487
BayerischeMotorenWerke <syaifulamri199@gmail.com>
2021-10-14 22:44:00 +0700

    courbet: change kernel config & switch to proton clang
    Signed-off-by: BayerischeMotorenWerke <syaifulamri199@gmail.com>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -26,7 +26,8 @@ ODM_MANIFEST_COURBET_FILES := \
     $(DEVICE_PATH)/configs/hidl/manifest-nfc.xml
 
 # Kernel
-TARGET_KERNEL_CONFIG := courbet_user_defconfig
+TARGET_KERNEL_CONFIG := courbet_defconfig
+TARGET_KERNEL_CLANG_VERSION := proton
 BOARD_KERNEL_CMDLINE += androidboot.selinux=permissive
 
 # Partitions
